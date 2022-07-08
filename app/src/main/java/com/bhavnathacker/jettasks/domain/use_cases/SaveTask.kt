6d3e40df4ddd23c2902af2000fef2207bda0ad1b
JasonHonor <jasonhonor@outlook.com>
2022-07-08 18:51:51 +0800

    Saved completed time.

--- app/src/main/java/com/bhavnathacker/jettasks/domain/use_cases/SaveTask.kt
+++ app/src/main/java/com/bhavnathacker/jettasks/domain/use_cases/SaveTask.kt
@@ -1,10 +1,15 @@
 package com.bhavnathacker.jettasks.domain.use_cases
 
 import com.bhavnathacker.jettasks.domain.model.Task
+import com.bhavnathacker.jettasks.domain.model.TaskStatus
 import com.bhavnathacker.jettasks.domain.repository.TaskRepository
+import java.util.*
 
 class SaveTask(val repository: TaskRepository) {
     suspend operator fun invoke(task: Task) {
+        if (task.status==TaskStatus.COMPLETED) {
+            task.completed = Date(Calendar.getInstance().timeInMillis);
+        }
         repository.saveTask(task)
     }
 }
