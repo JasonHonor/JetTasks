85366a907657189cc30428154aace84683bd0346
JasonHonor <jasonhonor@outlook.com>
2023-12-31 09:35:13 +0800

    get_apk_version support SDK condition.

--- app/src/main/java/com/bhavnathacker/jettasks/util/App.kt
+++ app/src/main/java/com/bhavnathacker/jettasks/util/App.kt
@@ -1,6 +1,7 @@
 package com.bhavnathacker.jettasks.util
 
 import android.content.Context
+import android.os.Build
 
 class App {
 
@@ -11,7 +12,12 @@ class App {
             val packageName = ctx.packageName
             val packageInfo = packageManager.getPackageInfo(packageName, 0)
             val versionName = packageInfo.versionName
-            val versionCode = packageInfo.longVersionCode
+            var versionCode = 0L
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                versionCode = packageInfo.longVersionCode
+            }else {
+                versionCode = packageInfo.versionCode.toLong()
+            }
             return Pair(versionName,versionCode)
         }
     }
